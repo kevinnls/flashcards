b3b98e129e9180696b0fba1886804dfb14a87166
kevinnls <developer@kevinnlsamuel.com>
2022-09-05 13:42:30 +0530

    more layout and transition; more sensible names

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 console.log('hello, world');
 
 const startBtn = document.getElementById('action-start')
+const stopBtn = document.getElementById('action-stop')
 const nextBtn = document.getElementById('action-next')
 const revealBtn = document.getElementById('action-reveal')
 const termBox = document.getElementById('term')
@@ -9,6 +10,10 @@ const explainDiag = document.getElementById('explanation')
 startBtn.addEventListener('click', () => {
 	console.log('hello')
 	document.querySelector('main').dataset.state = "playing";
+})
+stopBtn.addEventListener('click', () => {
+	console.log('hello')
+	document.querySelector('main').dataset.state = "stopped";
 })
 
 revealBtn.addEventListener('click', () => {
