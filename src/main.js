aaac32147b72d77d4fe3829a42624395a47f7dc1
kevinnls <developer@kevinnlsamuel.com>
2022-09-05 15:18:40 +0530

    better refs to DOMeles

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ console.log('hello, world');
 
 const startBtn = document.getElementById('action-start')
 const stopBtn = document.getElementById('action-stop')
+
+const prevBtn = document.getElementById('action-prev')
 const nextBtn = document.getElementById('action-next')
 const revealBtn = document.getElementById('action-reveal')
-const termBox = document.getElementById('term')
+
 const explainDiag = document.getElementById('explanation')
+
+const termBoxes = document.querySelectorAll('.term')
+const defnBoxes = document.querySelectorAll('.explanation')
 
 startBtn.addEventListener('click', () => {
 	console.log('hello')
