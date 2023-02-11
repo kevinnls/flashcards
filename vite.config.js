c0e0f93e2b8b550ad2e61d0c7ab5cd57c5dad9b1
kevinnls <developer@kevinnlsamuel.com>
2023-02-11 18:23:33 +0530

    try mpa building

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import { resolve } from 'path';
 export default {
 	base: "./",
 	build: {
 		target: "esnext",
 		polyfillModulePreload: false,
 		assetsInlineLimit: 768,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				home: resolve(__dirname, "home/index.html"),
+			}
+		}
 	},
 };
